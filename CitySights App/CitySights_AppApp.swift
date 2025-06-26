9447c36e3b594c6c9abb658f633b71d2a3afdedb
Jonathan Guerrero <jona.guerrero@live.com>
2025-06-25 22:18:13 -0500

    Initial Commit

---
+++ CitySights App/CitySights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySights_AppApp.swift
+//  CitySights App
+//
+//  Created by Jonathan Guerrero on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct CitySights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CitySights App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CitySights App
+//
+//  Created by Jonathan Guerrero on 6/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
