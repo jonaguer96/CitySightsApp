2d0d8df8d309192742584bd1bf1708743bd39e4a
Jonathan Guerrero <jona.guerrero@live.com>
2025-07-13 17:31:13 -0500

    Added list view, map view, and refactored

---
+++ CitySights App/HomeView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  CitySights App
+//
+//  Created by Jonathan Guerrero on 6/25/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @Environment(BusinessModel.self) var model
+    @State var selectedTab = 0
+    
+    var body: some View {
+        
+        @Bindable var model = model
+        
+        VStack {
+            HStack {
+                TextField("What are you looking for?", text: $model.query)
+                    .textFieldStyle(.roundedBorder)
+                
+                Button {
+                    // TODO: Implement Query
+                } label: {
+                    Text("Go")
+                        .padding(.horizontal)
+                        .padding(.vertical, 10)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+            
+            //Show picker
+            Picker ("", selection: $selectedTab) {
+                Text("List")
+                    .tag(0)
+                Text("Map")
+                    .tag(1)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            
+            //Show map or list
+            
+            if selectedTab == 1 {
+                MapView()
+            }
+            else {
+                ListView()
+            }
+            
+        }
+        .onAppear {
+            model.getBusinesses()
+        }
+        .sheet(item: $model.selectedBusiness) { item in
+            BusinessDetailView()
+        }
+        
+    }
+}
+
+#Preview {
+    HomeView()
+        .environment(BusinessModel())
+}
---
+++ CitySights App/MapView.swift
@@ -0,0 +1,19 @@
+//
+//  MapView.swift
+//  CitySights App
+//
+//  Created by Jonathan Guerrero on 7/13/25.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    MapView()
+}
