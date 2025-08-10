07350de3743eafc5edf088b198c45e9a29c1b108
Jonathan Guerrero <jona.guerrero@live.com>
2025-08-09 19:18:32 -0500

    Added feature to make phone calls and url to safari for the business selected.

--- CitySights App/BusinessDetailView.swift
+++ CitySights App/BusinessDetailView.swift
@@ -77,7 +77,14 @@ struct BusinessDetailView: View {
                     
                     HStack{
                         Image(systemName: "phone")
-                        Text(business?.phone ?? "")
+                        if let url = URL(string:"tel:\(business?.phone ?? "")") {
+                            Link(destination: url) {
+                                Text(business?.phone ?? "")
+                            }
+                        }
+                        else {
+                            Text(business?.phone ?? "")
+                        }
                         Spacer()
                         Image(systemName: "arrow.right")
                     }
@@ -88,8 +95,16 @@ struct BusinessDetailView: View {
                     
                     HStack{
                         Image(systemName: "globe")
-                        Text(business?.url ?? "")
-                            .lineLimit(1)
+                        if let url = URL(string:"\(business?.url ?? "")") {
+                            Link(destination: url) {
+                                Text(business?.url ?? "")
+                                    .lineLimit(1)
+                            }
+                        }
+                        else {
+                            Text(business?.url ?? "")
+                                .lineLimit(1)
+                        }
                         Spacer()
                         Image(systemName: "arrow.right")
                     }
@@ -101,8 +116,6 @@ struct BusinessDetailView: View {
                     HStack{
                         Image(systemName: "bubble.left.and.bubble.right")
                         Text("\(business?.reviewCount ?? 0) reivews")
-                        Spacer()
-                        Image(systemName: "arrow.right")
                     }
                     .padding(.top, 12)
                     
